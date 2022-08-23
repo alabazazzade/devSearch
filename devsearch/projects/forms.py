d4ceb260b18ee5a5a29fdbbc3e2986154dfb13d7
Ala <alalenik@gmail.com>
2022-08-23 23:58:37 +0430

    Theme installation

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title','desctiption','demo_link','source_link','tags','featured_image']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+      super(ProjectForm, self).__init__(*args, **kwargs)
+      self.fields['title'].widget.attrs.update({'class':'input'})  
+      self.fields['desctiption'].widget.attrs.update({'class':'input'})  
+      self.fields['demo_link'].widget.attrs.update({'class':'input'})  
+      self.fields['source_link'].widget.attrs.update({'class':'input'})  
+      self.fields['featured_image'].widget.attrs.update({'class':'input'})  
+    
